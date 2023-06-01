11dd348f4d2364eebaf71f0053519e894e606705
Matthijs Dorst <matthijsdorst@gmail.com>
2023-06-01 15:45:34 +0200

    Initial upload JAVA client

---
+++ ProjectorClient/src/main/java/projectorclient/controller/Controller.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projectorclient.controller;
+
+import javax.swing.JFrame;
+import projectorclient.view.ClientFrame;
+import projectorclient.model.Feed;
+import projectorclient.model.NetworkClient;
+
+/**
+ *
+ * @author Matthijs
+ */
+public class Controller {
+
+    public ClientFrame      gui;
+    public Feed             feedThread;
+    public NetworkClient    network;
+    
+    private static Controller instance;
+    
+
+    
+    public static Controller getInstance () {
+        if (instance == null)
+            instance = new Controller();
+        return instance;
+    }
+    
+    
+    private Controller () {
+        this.gui        = new ClientFrame();
+        this.feedThread = Feed.getInstance();
+        this.network    = NetworkClient.getInstance();
+    }
+
+    
+    public void startGui () throws Exception {
+        gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gui.setVisible(true);        
+    }
+    
+    
+    public void startFeed () throws Exception {
+        feedThread.start();
+    }
+}
---
+++ ProjectorClient/src/main/java/projectorclient/model/MaskSpot.java
@@ -0,0 +1,18 @@
+package projectorclient.model;
+
+/**
+ *
+ * @author Matthijs
+ */
+public class MaskSpot {
+    
+    
+    public int x, y, size;
+    
+    
+    public MaskSpot (int x, int y, int size) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+    }
+}
